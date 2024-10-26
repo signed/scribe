9a5faf4964797b4a52bc87175be9d2e4dad6d80b
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-10-26 10:31:23 +0200

    update dependencies

--- .release-it.cjs
+++ .release-it.cjs
@@ -1,11 +1,11 @@
 // https://github.com/release-it/release-it/blob/main/docs/configuration.md
 // https://github.com/release-it/release-it/blob/main/config/release-it.json
 module.exports = {
-    "git": {
-        "commitMessage": "release ${npm.name} ${version}",
-        "tagName": "${npm.name}@${version}",
-    },
-    "npm": {
-        "publish": true
-    }
+  git: {
+    commitMessage: 'release ${npm.name} ${version}',
+    tagName: '${npm.name}@${version}',
+  },
+  npm: {
+    publish: true,
+  },
 }
